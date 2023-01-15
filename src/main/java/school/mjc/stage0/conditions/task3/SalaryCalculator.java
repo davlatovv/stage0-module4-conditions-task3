0251dd8c3c200f56fcce510d1e5b08a40424a5d0
davlatovv <zafargithub@gmail.com>
2023-01-15 17:20:19 +0500

    Tasks with if-else

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -1,6 +1,21 @@
 package school.mjc.stage0.conditions.task3;
 
+import javax.swing.*;
+
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        if(salary <= 10000){
+            float without_taxes = (float) ((float) salary-(salary * 0.15));
+            System.out.println(without_taxes);
+        } else if (10000 < salary && salary <= 20000)  {
+            float without_taxes = (float) ((float) salary-(salary * 0.18));
+            System.out.println(without_taxes);
+        } else if (salary > 20000) {
+            float without_taxes = (float) ((float) salary-(salary * 0.2));
+            System.out.println(without_taxes);
+        }
+        else {
+            System.out.println("wrong input!");
+        }
     }
 }
